5db136ca9661e403e9de27a1520c8f541194f4e4
glqdlt <dlfdnd0725@gmail.com>
2021-05-25 15:58:01 +0900

    Init

---
+++ src/main/java/com/glqdlt/ex/servicebusexample/ServiceBusExampleApplication.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.servicebusexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBusExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBusExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glqdlt/ex/servicebusexample/ServiceBusExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.servicebusexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBusExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
